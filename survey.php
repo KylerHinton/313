454c56827bdacc07e8051543da2eb3355ecc06bf
Similoski <kylerhinton@yahoo.com>
2014-10-02 12:30:23 -0700

    fixed session 3

--- survey.php
+++ survey.php
@@ -3,16 +3,25 @@ session_start();
 // store session data
 $_SESSION['game'] = 0; //$_REQUEST['game'];
 ?>
+<?php
+setcookie("game", "game", time()+3600);
+?>
 <html>
 <body>
 
+<?php
+// Print a cookie
+echo $_COOKIE["game"];
 
+// A way to view all cookies
+print_r($_COOKIE);
+?>
 <?php
 //retrieve session data
 echo "The most popular types of games are:";
 ?><br/>
 <?php
-echo "Side Scroller: ". $_SESSION['game'] +1;
+echo "Side Scroller: ". $_SESSION['game'];
 ?><br/>
 <?php
 echo "MMORPG: ". $_SESSION['game'];
