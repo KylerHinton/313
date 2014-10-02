9540789c9a2f9c4c78797e93b7fc898942cd32d1
Similoski <kylerhinton@yahoo.com>
2014-10-02 11:23:24 -0700

    fixed session 2

--- survey.php
+++ survey.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // store session data
-$_SESSION['game']=1;
+$_SESSION['game'];
 ?>
 <html>
 <body>
@@ -15,13 +15,13 @@ echo "The most popular types of games are:";
 echo "Side Scroller". $_SESSION['game'];
 ?><br/>
 <?php
-echo "MMORPG". $_SESSION['game'];
+echo "MMORPG". $_SESSION['game1'];
 ?><br/>
 <?php
-echo "RTS". $_SESSION['game'];
+echo "RTS". $_SESSION['game2'];
 ?><br/>
 <?php
-echo "RPG". $_SESSION['game'];
+echo "RPG". $_SESSION['game3'];
 ?>
 
 <br>
