296cdd136a44090609a423ab7fc7e9f4a3213ed0
Similoski <kylerhinton@yahoo.com>
2014-10-02 11:01:01 -0700

    added session

--- survey.php
+++ survey.php
@@ -1,6 +1,16 @@
+<?php
+session_start();
+// store session data
+$_SESSION['views']=1;
+?>
 <html>
 <body>
 
+
+<?php
+//retrieve session data
+echo "Pageviews=". $_SESSION['views'];
+?>
 <?php
 	echo $_POST["game"];
 ?>
