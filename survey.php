c3f58c92f8e0c9d59033b0c4504b17e0caaef1d3
Similoski <kylerhinton@yahoo.com>
2014-10-02 12:37:25 -0700

    fixed session 3

--- survey.php
+++ survey.php
@@ -1,21 +1,35 @@
 <?php
 session_start();
 // store session data
-$_SESSION['game'] = 0; //$_REQUEST['game'];
+$_SESSION['game'] = 0;//$_REQUEST['game'];
+$_SESSION['game1'] = 0;
+$_SESSION['game2'] = 0;
+$_SESSION['game3'] = 0;
+if(isset($_SESSION['game']))
+$_SESSION['game']=$_SESSION['game']+1;
+else
+$_SESSION['game']=1;
+echo "Views=". $_SESSION['game'];
+if(isset($_SESSION['game1']))
+$_SESSION['game1']=$_SESSION['game1']+1;
+else
+$_SESSION['game1']=1;
+echo "Views=". $_SESSION['game1'];
+if(isset($_SESSION['views']))
+$_SESSION['game2']=$_SESSION['game2']+1;
+else
+$_SESSION['game2']=1;
+echo "Views=". $_SESSION['game2'];
+if(isset($_SESSION['game3']))
+$_SESSION['game3']=$_SESSION['game3']+1;
+else
+$_SESSION['game3']=1;
+echo "Views=". $_SESSION['game3'];
 ?>
-<?php
-setcookie("game", "game", time()+3600);
-?>
+
 <html>
 <body>
 
-<?php
-// Print a cookie
-echo $_COOKIE["game"];
-
-// A way to view all cookies
-//print_r($_COOKIE);
-?>
 <?php
 //retrieve session data
 echo "The most popular types of games are:";
