62a7a2fd35c84636f9fbe7013c6346c531b87ab8
Similoski <kylerhinton@yahoo.com>
2014-10-04 12:15:02 -0700

    fixed survey.php

--- survey.php
+++ survey.php
@@ -44,6 +44,27 @@ echo "Views=". $_SESSION['game3'];*/
 // Set session variables
 $_SESSION["game"] = 1;
 
+
+if(isset($_SESSION['game']))
+$_SESSION['game']=$_SESSION['game']+1;
+else
+$_SESSION['game']=1;
+echo "Views=". $_SESSION['game'];
+if(isset($_SESSION['game1']))
+$_SESSION['game1']=$_SESSION['game1']+1;
+else
+$_SESSION['game1']=1;
+echo "Views=". $_SESSION['game1'];
+if(isset($_SESSION['views']))
+$_SESSION['game2']=$_SESSION['game2']+1;
+else
+$_SESSION['game2']=1;
+echo "Views=". $_SESSION['game2'];
+if(isset($_SESSION['game3']))
+$_SESSION['game3']=$_SESSION['game3']+1;
+else
+$_SESSION['game3']=1;
+echo "Views=". $_SESSION['game3'];
 ?>
 <?php
   if($_POST['submit'] == "Submit") 
@@ -76,7 +97,7 @@ echo "Side Scroller: ". $_SESSION['game'];// = $_REQUEST['game'];
 	}
 ?>
 <?php
-echo "MMORPG: ". $_SESSION['game'] + 1;// = $_REQUEST['game'];
+echo "MMORPG: ". $_SESSION['game'];// = $_REQUEST['game'];
 ?><br/>
 <?php
 echo "RTS: ". $_SESSION['game'];// = $_REQUEST['game'];
