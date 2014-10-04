f3fb7dfae597788d64c5309bfa9387af374b7d47
Similoski <kylerhinton@yahoo.com>
2014-10-04 12:28:42 -0700

    fixed survey.php

--- survey.php
+++ survey.php
@@ -51,22 +51,22 @@ echo "Views=". $_SESSION['game3'];*/
 if(isset($_SESSION['game']))
 $_SESSION['game']=$_SESSION['game']+1;
 else
-$_SESSION['game']=1;
+$_SESSION['game']=0;
 echo "Views=". $_SESSION['game'];
 if(isset($_SESSION['game1']))
 $_SESSION['game1']=$_SESSION['game1']+1;
 else
-$_SESSION['game1']=1;
+$_SESSION['game1']=0;
 echo "Views=". $_SESSION['game1'];
 if(isset($_SESSION['views']))
 $_SESSION['game2']=$_SESSION['game2']+1;
 else
-$_SESSION['game2']=1;
+$_SESSION['game2']=0;
 echo "Views=". $_SESSION['game2'];
 if(isset($_SESSION['game3']))
 $_SESSION['game3']=$_SESSION['game3']+1;
 else
-$_SESSION['game3']=1;
+$_SESSION['game3']=0;
 echo "Views=". $_SESSION['game3'];
 ?>
 <?php
