0d27b80e1ec1661811be12204cce2e4996ef96dd
Similoski <kylerhinton@yahoo.com>
2014-10-16 23:58:53 -0700

    database access

--- database.php
+++ database.php
@@ -26,7 +26,7 @@ $gamers = $statement->fetchAll(PDO::FETCH_ASSOC);
 echo "<html><body>";
 foreach($gamers AS $games)
 {
-echo "ID: ".$games['game_id']."<br /> Username:".$games['username']."<br /> Game Name: ".$games['game_name'].' <br /> System: '.$games['system_name']."<br />Hours played: ".$games['hours_played']."<br />Game Finished:".$games['completed']."<br />";
+echo "<b>ID: </b>".$games['game_id']."<br /><b>Username:</b> ".$games['username']."<br /><b>Game Name: </b>".$games['game_name'].' <br /><b>System:</b> '.$games['system_name']."<br /><b>Hours played:</b> ".$games['hours_played']."<br /><b>Game Finished:</b> ".$games['completed']."<br />";
 }
 echo "</body></html>"
 ?>​
