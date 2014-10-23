dd8d9313c664c8939715a02e6a6cf81fd0eb7ae6
Similoski <kylerhinton@yahoo.com>
2014-10-23 11:09:14 -0700

    added insertGames_music.php

--- insertGames_music.php
+++ insertGames_music.php
@@ -28,11 +28,11 @@ try{
         $error_message = $PDOException->getMessage();
         echo $error_message;   
     }
-    $sql ="SELECT * FROM game_images";
+    $sql ="SELECT * FROM game_music";
     $statement = $db->prepare($sql);
     $statement->execute();
  	$gamers = $statement->fetchAll(PDO::FETCH_ASSOC);
- 	echo"<html><body style='background-color:fuchsia';>";
+ 	echo"<html><body style='background-color:olive';>";
 	foreach($gamers AS $game_music)
 	{
 	echo "<b>Title: </b>".$game_music['title']."<br /><b>Song Length:</b> ".$game_music['length']."<br /><b>Game Location of Music: </b>".$game_music['ingame_loc'].' <br /><b>Artists Name:</b> '.$game_music['artist_name']."<br /><br />";
